efdb44ade6dde1dfe2b858be2fd998077bf5752e
Jayres18 <jaylansang07@gmail.com>
2025-11-28 11:28:58 +0800

    API Key configured. Database Model configured. Image upload Implemented

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,4 +1,6 @@
 import connectDB from "@/lib/mongodb";
+import { v2 as cloudinary } from "cloudinary";
+
 import { NextRequest, NextResponse } from "next/server";
 import Event from "@/database/event.model";
 
@@ -19,6 +21,31 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    const file = formData.get("image") as File;
+
+    if (!file)
+      return NextResponse.json(
+        { message: "Image file is required" },
+        { status: 400 }
+      );
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "BatStateU-Events" },
+          (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
 
     return NextResponse.json(
@@ -32,6 +59,24 @@ export async function POST(req: NextRequest) {
         message: "Event Creation Failed",
         error: e instanceof Error ? e.message : "Unknown Error",
       },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { message: "Events Fetched Successfully", events },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Event fetching Failed", error: e },
       { status: 500 }
     );
   }
